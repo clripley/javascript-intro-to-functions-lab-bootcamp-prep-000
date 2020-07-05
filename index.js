05691b47a58abeaa53c3fd30dae32480af785e5e
Christopher Ripley <clripley@gmail.com>
2020-07-05 00:29:30 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -21,5 +21,7 @@ if (string.toLowerCase() === string){
 return ('I can\'t hear you!');
 } if (string.toUpperCase() === string){
 return ('YES INDEED!');
+} if (string === 'I love you, Grandma.'){
+  return ('I Love you, too.')
 }
 }
